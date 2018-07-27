1e33f271c1f93abb6e4d9b3dd5d6a071464e913a
Leader21323123 <41390048+Leader21323123@users.noreply.github.com>
2018-07-27 20:19:10 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -78,4 +78,30 @@ client.on("message", message => {
             return;
         }
     });
+client.on('message', message => {
+    if (message.content.startsWith("رابط")) {
+        
+  message.channel.createInvite({
+        thing: true,
+        maxUses: 5,
+        maxAge: 86400
+    }).then(invite =>  
+      message.author.sendMessage(invite.url)
+    )
+    const embed = new Discord.RichEmbed()
+        .setColor("2fff00")
+        .setDescription("| :white_check_mark:  | :heart:  تم ارسال الرابط على الخاص  ")
+        .setFooter("Country - AH")
+      message.channel.sendEmbed(embed).then(message => {message.delete(10000)})
+              const Embed11 = new Discord.RichEmbed()
+        .setColor("2fff00")
+        .setDescription(`
+**-------------------
+-هذا هو الرابط 
+-ارسله للي تحب وحيآك انت وياه
+-ونورنا ياجميل :heart: 
+------------------- **`)
+      message.author.sendEmbed(Embed11)
+    }
+});
 client.login(process.env.BOT_TOKEN);
